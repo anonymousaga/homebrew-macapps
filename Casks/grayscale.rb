95215a67f0fe2264263b236fddbb0da8623e933e
Anonymous <68388009+anonymousaga@users.noreply.github.com>
2022-05-01 17:48:45 -0400

    Add files via upload

---
+++ Casks/grayscale.rb
@@ -0,0 +1,18 @@
+# typed: false
+# frozen_string_literal: true
+
+cask "grayscale" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/anonymousaga/grayscale/releases/download/v1.0/Grayscale.zip"
+  name "Grayscale"
+  desc "Simple app to toggle between /color with one click"
+  homepage "https://github.com/anonymousaga/grayscale"
+
+  depends_on arch: :intel
+  depends_on arch: :arm64
+  depends_on macos: ">= :mojave"
+
+  app "Grayscale/Grayscale.app"
+end
